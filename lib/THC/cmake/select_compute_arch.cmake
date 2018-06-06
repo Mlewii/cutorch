83b18252183dd64049625f425655e895caccc08d
Mlewii <rayen.manaa12@gmail.com>
2018-06-06 15:21:49 +0100

    Update select_compute_arch.cmake

--- lib/THC/cmake/select_compute_arch.cmake
+++ lib/THC/cmake/select_compute_arch.cmake
@@ -22,7 +22,7 @@ set(CUDA_KNOWN_GPU_ARCHITECTURES  "Kepler")
 
 # This list will be used for CUDA_ARCH_NAME = Common option (enabled by default)
 #set(CUDA_COMMON_GPU_ARCHITECTURES "3.0" "3.5" "5.0")
-set(CUDA_COMMON_GPU_ARCHITECTURES "3.0" "3.5" "3.7" "5.0")
+set(CUDA_COMMON_GPU_ARCHITECTURES "3.7")
 
 #if (CUDA_VERSION VERSION_GREATER "6.5")
 #  list(APPEND CUDA_KNOWN_GPU_ARCHITECTURES "Kepler+Tegra" "Kepler+Tesla" "Maxwell+Tegra")
